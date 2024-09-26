ee3df5e58e5ae3cda5b57a0df0d543d020d4f711
Suporte Técnico Elo <suporte@DESKTOP-FOGM3RP>
2024-09-26 11:09:45 -0300

    estrututra

---
+++ src/App.js
@@ -0,0 +1,23 @@
+function copyText(elementId) {
+  var text = document.getElementById(elementId).innerText;
+  navigator.clipboard.writeText(text).then(function () {
+
+  }, function (err) {
+    alert("Erro ao copiar texto: " + err);
+  })
+}
+
+now = new Date
+let mensagem
+if (now.getHours() >= 0 && now.getHours() < 12) {
+  mensagem = "Bom dia"
+} else if (now.getHours() >= 12 && now.getHours() < 18) {
+  mensagem = "Boa tarde"
+} else if (now.getHours() >= 18 && now.getHours() < 23) {
+  mensagem = "Boa noite"
+}
+
+for (let i = 0; i < 50; i++) {
+  let idnumber = i + 1
+  document.getElementById("cumprimento" + idnumber).innerText = mensagem
+}
