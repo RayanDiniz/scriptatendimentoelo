6dceda2fdcd288d1683fa49d68d779dac58974e9
Rayan Diniz <rayabncassiokiol@gmail.com>
2024-11-11 22:53:23 -0300

    Construção da api e mudança do front

--- api/models/Frase.js
+++ api/models/Frase.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const FraseSchema = new mongoose.Schema({
-  id: { type: String, required: true },
+  id: { type: String },
   titulo: { type: String, required: true },
   autor: { type: String, required: true },
   textos: { type: [String], required: true }
